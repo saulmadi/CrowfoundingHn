40ae2e3f6d922a89df669e89540a0293152c2fee
Saul Mayorquin <saul@acklenavenue.com>
2014-05-24 12:04:41 -0600

    Adding domain project specs

---
+++ src/CrowfoundingHn.Projects.Specs/Application/when_creating_a_new_project.cs
@@ -0,0 +1,13 @@
+﻿using Machine.Specifications;
+
+namespace CrowfoundingHn.Projects.Specs.Application
+{
+    public class when_creating_a_new_project
+    {
+        Establish context = () => { };
+
+        Because of = () =>{};
+
+        It should_create_the_new_project = () => { };
+    }  
+}
