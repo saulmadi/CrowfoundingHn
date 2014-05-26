3e7035bf46dea1ab0b9217654b4a165890784324
Saul Mayorquin <saul@acklenavenue.com>
2014-05-26 00:30:23 -0600

    Adding Project

---
+++ src/CrowfoundingHn.Presentation/App/Services/ProjectService.js
@@ -0,0 +1,18 @@
+﻿angular.module("crowfoundingHn").factory("ProjectService", ["$http", function($http) {
+
+    var factory = {};
+    var getHeaders = function () {
+        return {
+            "Content-Type": "application/json"
+        };
+    };
+    factory.createProject = function(project) {
+        return $http({
+            url: "/projects",
+            method: "POST",
+            data: project,
+            headers: getHeaders()
+        });
+    };
+    return factory;
+}]);
